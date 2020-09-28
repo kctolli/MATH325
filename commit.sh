36555059e0a011bf72c455f6c2f8ca093b305870
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-09-27 22:28:54 -0600

    Update at 22:28 on 09/27/2020

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd D:/MATH325
+cd G:/MATH325
 git add --all
 timestamp() {
   date +"at %H:%M on %m/%d/%Y"
@@ -7,15 +7,3 @@ timestamp() {
 git commit -am "Update $(timestamp)"
 git pull
 git push origin master
-
-# Log the commit
-
-repo="Machine Learning"
-
-## Declare file
-cd C:/RunFiles
-log=commit_log.txt
- 
-## append commit to log file
-printf "Commit ${repo} $(timestamp)" >> $log
-printf "\n" >> $log
